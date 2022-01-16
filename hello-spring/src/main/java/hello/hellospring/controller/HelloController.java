57c478257e0291caab0c6db14311db9090d74191
bopi447 <bopi447@naver.com>
2022-01-16 15:36:27 +0900

    spring make repository

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,8 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 
@@ -13,4 +15,43 @@ public class HelloController {
         model.addAttribute("data","kim!!");
         return "hello"; //resources의 templates의 ~~.html로 이동.
     }
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name",required = false) String name, Model model){
+        model.addAttribute("name",name);
+        return "hello-template";
+    }
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name){
+        return "hello"+name; // "hello spring"
+    }
+
+    @GetMapping("test")
+    @ResponseBody
+    public String test(@RequestParam("id") String name,@RequestParam("password") String pwd){
+        return name + pwd;
+    }
+
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+
+        return hello;
+    }
+
+    static class Hello{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
 }
